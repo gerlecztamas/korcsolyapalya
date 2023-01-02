5489ae3fe63701e57bff5e3ac90180a51d4be49b
Hadri2002 <horvathadri006@gmail.com>
2023-01-02 15:16:56 +0100

    xml reader implementation

--- src/main/java/Model/RequestModel.java
+++ src/main/java/Model/RequestModel.java
@@ -4,22 +4,20 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import Model.XmlWriter;
 
+import java.io.File;
+
+
 public class RequestModel {
 
     public static String kolcsonzes(JSONObject igeny){
         String result = "Nincs az igényeknek megfelelő korcsolya!";
 
-        //TEST LISTA, MEG KELL HOGY KAPJA JSONARRAYBEN AZ ÖSSZES KORI ADATÁT:
-        JSONArray korcsolyak = new JSONArray();
-        Korcsolya korcsolya1 = new Korcsolya(1, KorcsolyaTipusEnum.HockeyKorcsolya, 36, "kék");
-        Korcsolya korcsolya2 = new Korcsolya(2, KorcsolyaTipusEnum.Mukorcsolya, 38, "piros");
-        korcsolyak.put(korcsolya1.toJson());
-        korcsolyak.put(korcsolya2.toJson());
+        JSONArray korcsolyak = XmlReader.read(System.getProperty("user.dir") + "\\IdeaProjects\\korcsolyapalya\\src\\main\\resources\\korcsolyak.xml");
 
         for(int i = 0; i < korcsolyak.length(); i++){
             JSONObject korcsolya = korcsolyak.getJSONObject(i);
             try{
-                if(korcsolya.get("meret") == igeny.get("meret") && korcsolya.getString("tipus").equals(igeny.getString("tipus"))){
+                if(korcsolya.getString("meret").equals(igeny.getString("meret")) && korcsolya.getString("tipus").equals(igeny.getString("tipus"))){
 
                     Korcsolya kolcsonzott = new Korcsolya(3, KorcsolyaTipusEnum.valueOf(korcsolya.getString("tipus")), korcsolya.getInt("meret"), korcsolya.getString("szin"));
                     result = "A következő korcsolyát sikeresen kikölcsönözte: \n"+ kolcsonzott.toString();
