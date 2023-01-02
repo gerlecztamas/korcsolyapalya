3256f3bbb37fdd429c37023bd90ace83c2fbf2de
Hadri2002 <horvathadri006@gmail.com>
2023-01-02 13:06:36 +0100

    generics xml writer usage implementation

--- src/main/java/Model/Tranzakcio.java
+++ src/main/java/Model/Tranzakcio.java
@@ -7,23 +7,15 @@ public class Tranzakcio {
     private String vezeteknev;
     private String keresztnev;
     private String email;
-    private LocalDateTime kezdet;
-    private LocalDateTime veg;
+
 
     public Tranzakcio() {
     }
-    public Tranzakcio(int id, String vezeteknev, String keresztnev, String email, LocalDateTime kezdet, LocalDateTime veg) {
+    public Tranzakcio(int id, String vezeteknev, String keresztnev, String email) {
         this.id = id;
         this.vezeteknev = vezeteknev;
         this.keresztnev = keresztnev;
         this.email = email;
-        this.kezdet = kezdet;
-        if(veg.isAfter(kezdet)){
-            this.veg = veg;
-        }
-        else {
-            this.veg = kezdet;
-        }
     }
 
     public int getId() {
@@ -42,11 +34,4 @@ public class Tranzakcio {
         return email;
     }
 
-    public LocalDateTime getKezdet() {
-        return kezdet;
-    }
-
-    public LocalDateTime getVeg() {
-        return veg;
-    }
 }
